28e8f43679fc9e8c1fde87a3ffb59fe714067186
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2023-10-28 22:03:42 +0200

    Improve printing game stats in GameSeriesSimulator

--- src/main/kotlin/GameSeriesSimulator.kt
+++ src/main/kotlin/GameSeriesSimulator.kt
@@ -1,9 +1,9 @@
-private const val count = 5
+private const val count = 10
 
 fun main() {
-    val player1Strategy = Player("Simple", SimpleStrategy)
-    val player2Strategy = Player("Optimal", OptimalStrategy)
-    val players = listOf(player1Strategy, player2Strategy)
+    val player1 = Player("Simple", SimpleStrategy)
+    val player2 = Player("Optimal", OptimalStrategy)
+    val players = listOf(player1, player2)
     val game = Game(players)
     val results = mutableListOf<Map<Player, Int>>()
     for (i in 0 until count) {
@@ -16,4 +16,25 @@ fun main() {
         }
         println()
     }
+    val (wins, draws, loses) = results.getStats(player2)
+    println("${player2.name} wins: $wins, draws: $draws, loses: $loses")
+}
+
+private fun List<Map<Player, Int>>.getStats(player: Player): Triple<Int, Int, Int> {
+    var wins = 0
+    var draws = 0
+    var loses = 0
+    for (game in this) {
+        val otherPlayers = game.filterKeys { it != player }
+        val playerScore = game.getValue(player)
+        val maxOpponentScore = otherPlayers.maxOf { (_, points) -> points }
+        if (playerScore > maxOpponentScore) {
+            wins += 1
+        } else if (playerScore == maxOpponentScore) {
+            draws += 1
+        } else {
+            loses += 1
+        }
+    }
+    return Triple(wins, draws, loses)
 }
