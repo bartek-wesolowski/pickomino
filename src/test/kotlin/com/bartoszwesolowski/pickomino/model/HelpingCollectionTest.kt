47c67ded09efbe4ddd853b540508c0b0dcbd9856
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2023-11-17 16:03:44 +0100

    Remove unused imports from HelpingCollectionTest

--- src/test/kotlin/com/bartoszwesolowski/pickomino/model/HelpingCollectionTest.kt
+++ src/test/kotlin/com/bartoszwesolowski/pickomino/model/HelpingCollectionTest.kt
@@ -1,7 +1,5 @@
 package com.bartoszwesolowski.pickomino.model
 
-import com.bartoszwesolowski.pickomino.model.Helping
-import com.bartoszwesolowski.pickomino.model.HelpingCollection
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
