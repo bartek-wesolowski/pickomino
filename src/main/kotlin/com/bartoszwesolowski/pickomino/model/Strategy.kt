2ee2504423b99b5853a8ec4f7903c80933fdb156
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2023-12-04 20:33:39 +0100

    Add comments to Strategy

--- src/main/kotlin/com/bartoszwesolowski/pickomino/model/Strategy.kt
+++ src/main/kotlin/com/bartoszwesolowski/pickomino/model/Strategy.kt
@@ -1,12 +1,15 @@
 package com.bartoszwesolowski.pickomino.model
 
+/** A strategy of playing the game. */
 interface Strategy {
+    /** Returns a side to choose based on the current game state or null if no side can be chosen. */
     fun chooseSide(
         gameState: GameState,
         turnState: TurnState,
         roll: Roll
     ): Side?
 
+    /** Returns whether you should continue rolling the dice or not. */
     fun shouldContinue(
         gameState: GameState,
         turnState: TurnState
