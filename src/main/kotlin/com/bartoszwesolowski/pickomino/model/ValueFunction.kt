2efc7a90660dd5ee1e667c3b49392d8cc5bfd20c
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2023-12-04 20:45:48 +0100

    Add comments to ValueFunction

--- src/main/kotlin/com/bartoszwesolowski/pickomino/model/ValueFunction.kt
+++ src/main/kotlin/com/bartoszwesolowski/pickomino/model/ValueFunction.kt
@@ -1,6 +1,9 @@
 package com.bartoszwesolowski.pickomino.model
 
+/** A function that assigns a value to a certain amount of points in a game state. */
 interface ValueFunction {
+    /** The range of possible values. */
     val valueRange: IntRange
+    /** Returns the value of a certain amount of points in a game state. */
     fun getValue(gameState: GameState, points: Int): Int
 }
