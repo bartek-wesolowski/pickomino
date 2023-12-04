59227f68b07510fbdb354b6cd83debe489a131d9
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2023-12-04 20:40:35 +0100

    Add comments to ResultDistribution

--- src/main/kotlin/com/bartoszwesolowski/pickomino/model/ResultDistribution.kt
+++ src/main/kotlin/com/bartoszwesolowski/pickomino/model/ResultDistribution.kt
@@ -1,5 +1,9 @@
 package com.bartoszwesolowski.pickomino.model
 
+/**
+ * A distribution of results of a game.
+ * It's essentially a map, but there are different implementations for performance reasons.
+ */
 interface ResultDistribution : Iterable<Map.Entry<Int, Double>> {
     operator fun get(value: Int): Double
     fun getExpectedValue(): Double
